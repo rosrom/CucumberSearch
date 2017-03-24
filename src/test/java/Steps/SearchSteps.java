62d9d5b136619efa4af358260fc87b4437c61820
Rosa Roman Alvarez <rosaromanalvarez@gmail.com>
2017-03-24 11:44:50 +0100

    Add Assertion and Open browser in BaseUtil

--- src/test/java/Steps/SearchSteps.java
+++ src/test/java/Steps/SearchSteps.java
@@ -4,20 +4,31 @@ import Base.BaseUtil;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * Created by rosroman on 23/03/17.
  */
+
+
 public class SearchSteps extends BaseUtil {
+
+    private Base.BaseUtil base;
+
+    public SearchSteps(BaseUtil base) {
+        this.base = base;
+    }
+
     @Given("^I navigate to google page$")
     public void iNavigateToGooglePage() throws Throwable {
 
         //I open browser in here, as it is not working in the Hook
         //Driver = new FirefoxDriver();
-    //    Driver.navigate().to("http://www.google.es");
+        base.Driver.navigate().to("http://www.google.es");
         System.out.println("I navigate to google page\n");
     }
 
@@ -25,7 +36,7 @@ public class SearchSteps extends BaseUtil {
     public void iEnterInFieldSearch(String searchterm) throws Throwable {
 
 
-        Driver.findElement(By.id("lst-ib")).sendKeys(searchterm);
+        base.Driver.findElement(By.id("lst-ib")).sendKeys(searchterm);
         System.out.println("The term I want to search is: " + searchterm);
 
 
@@ -33,12 +44,17 @@ public class SearchSteps extends BaseUtil {
 
     @And("^I click search button$")
     public void iClickSearchButton() throws Throwable {
-        Driver.findElement(By.id("lst-ib")).submit();
+        base.Driver.findElement(By.id("lst-ib")).submit();
 
     }
 
     @Then("^I should see the results page$")
+
     public void iShouldSeeTheResultsPage() throws Throwable {
+        
+        base.Driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Assert.assertEquals("It's not displayed", base.Driver.findElement(By.id("resultStats")).isDisplayed(), true);
+
 
     }
 }
